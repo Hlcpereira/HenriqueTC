0e2f1c4fdc9db38fbcd2331d58a0d4f3fa7422e0
John Laff <johnlaffstudio@protonmail.com>
2019-05-20 18:56:07 +0000

    ó o sequestro
    Co-authored-by: dattebayorob <robson.william65@gmail.com>

---
+++ sequestro.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+echo "Ｈｅｎｒｉｑｕｅ, [19.05.19 17:41]
+Esfrega na cara do esrom
+
+Ｈｅｎｒｉｑｕｅ, [19.05.19 17:41]
+Aquele filho da puta
+
+Ｈｅｎｒｉｑｕｅ, [19.05.19 17:41]
+Vou pagar alguém pra sequestrar aquele filho da puta
+
+Ｈｅｎｒｉｑｕｅ [19.05.19 17:41]
+Ninguém presta
+
+Ｈｅｎｒｉｑｕｅ, [19.05.19 17:41]
+Call90 n presta
+
+Ｈｅｎｒｉｑｕｅ, [19.05.19 17:41]
+Esrom n presta
+
+Ｈｅｎｒｉｑｕｅ, [19.05.19 17:41]
+92% dos devs Br n presta"
